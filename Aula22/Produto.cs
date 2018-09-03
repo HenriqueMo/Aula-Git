d47961611e1c56f3a6cf15ecc0cf09357c66dd60
Henrique Moreira <Henrique@farmaciaindiana.com.br>
2018-09-03 17:31:10 -0300

    Projeto teste

---
+++ Aula22/Produto.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace curso
+{
+    class Produto
+    {
+        public string nome;
+        public double preco;
+        public int quantidadeEmEstoque;
+
+        public Produto(string nome, double preco, int quantidadeEmEstoque)  // o CONSTRUTOR sempre com o nome da CLASSE
+        {
+            this.nome = nome;  // o THIS esta referenciando o OBEJETO nome = nome
+            this.preco = preco;
+            this.quantidadeEmEstoque = quantidadeEmEstoque;
+        }
+
+        public Produto(string nome, double preco)  // o CONSTRUTOR com SOBRECARGA, foi tirado o parametro int quantidadeEmEstoque
+        {
+            this.nome = nome;  // o THIS esta referenciando o OBEJETO nome = nome
+            this.preco = preco;
+            this.quantidadeEmEstoque =0;
+        }
+
+        public double valorTotalEmEstoque() // metodo
+        {
+            return preco * quantidadeEmEstoque;
+        }
+        public void realizarEntrada(int quantidade) // o metodo VOID não tem retorno
+        {
+            quantidadeEmEstoque = quantidadeEmEstoque + quantidade;
+        }
+        public void realizarSaida(int quantidade)
+        {
+            quantidadeEmEstoque = quantidadeEmEstoque - quantidade;
+        }
+
+    }
+}
---
+++ Aula22/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Globalization;
+
+namespace curso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int qte;
+            Produto P;
+           
+
+            Console.WriteLine("Digite os dados do produto: ");
+            Console.Write("Nome:  ");
+            string nome = Console.ReadLine();
+            Console.Write("Preço:  ");
+            double preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            //Console.Write("Quantidade em Estoque:  ");   para usar o construtor de SOBRECARGA
+            //int quantidade = int.Parse(Console.ReadLine());
+
+            P = new Produto(nome, preco);  // instanciar PRODUTO
+
+            Console.WriteLine();
+            Console.WriteLine("Dados do Produto: " + P.nome + ", R$" + P.preco + ", " + P.quantidadeEmEstoque + ", unidades, Total: R$: "
+                + P.valorTotalEmEstoque());
+
+            Console.WriteLine();
+            Console.Write("Digite a quantidade de produtos que entraram no estoque: ");
+
+            qte = int.Parse(Console.ReadLine());
+            P.realizarEntrada(qte); // chamando o método RealizarEntrada
+
+            Console.WriteLine();
+            Console.WriteLine("Dados atualizados: " + P.nome + ", R$" + P.preco + ", " + P.quantidadeEmEstoque + ", unidades, Total: R$: "
+                + P.valorTotalEmEstoque());
+                
+            // Sairam do ESTOQUE
+            Console.WriteLine();
+            Console.Write("Digite a quantidade de produtos que Sairam do estoque: ");
+
+            qte = int.Parse(Console.ReadLine());
+            P.realizarSaida(qte); // chamando o método RealizarSaida
+
+            Console.WriteLine();
+            Console.WriteLine("Dados atualizados: " + P.nome + ", R$" + P.preco + ", " + P.quantidadeEmEstoque + ", unidades, Total: R$: "
+                + P.valorTotalEmEstoque());
+
+
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
